565fe6612c7aa758d2d4b07f94bc2101ce0aba30
zhaosanyin <zhaosan.yin@gmail.com>
2023-03-25 15:45:02 +0000

    including emoji adding method

---
+++ assertEqual.js
@@ -0,0 +1,22 @@
+// FUNCTION IMPLEMENTATION
+//const actual =1 ;
+//const expected =2;
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return (console.log(String.fromCodePoint('0x2705') + "Assertion Passed:" + actual + " === " + expected));
+  } else {
+    return (console.log(String.fromCodePoint('0x274c') + "Assertion failed:" + actual + " !=== " + expected));
+  }
+};
+
+
+//console.assert(assertEqual("lighthouse", "lighthouse"));
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 7);
+
+
+
+//tring.fromCodePoint('0x2705') add the emoji symbol
