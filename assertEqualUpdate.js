03999d50cd2cd9ebff1a9a0919cb8b4102f5ee8c
zhaosanyin <zhaosan.yin@gmail.com>
2023-03-29 14:14:03 +0000

    update

--- assertEqualUpdate.js
+++ assertEqualUpdate.js
@@ -1,4 +1,4 @@
- const eqArray = function(actual, expected) {
+/*const eqArray = function(actual, expected) {
   if (actual.length === expected.length && actual.every((element, index)=>element === expected[index])) {
     return (console.log(String.fromCodePoint('0x2705') + "Assertion Passed:" + actual + " === " + expected));
   } else {
@@ -8,14 +8,24 @@
 
 
 //eqArray([1, 2, 3], [1, 2, 3]);
-/*
+
 eqArray([1, 2, 3], [1, 2, 3]); // => true
 eqArray([1, 2, 3], [3, 2, 1]); // => false
 
 eqArray(["1", "2", "3"], ["1", "2", "3"]); // => true
 eqArray(["1", "2", "3"], ["1", "2", 3]); // => false */
 
-eqArray("Lighthouse Labs", "Bootcamp");
-eqArray(1, 1);
-eqArray("Lighthouse Labs", "Lighthouse Labs");
-eqArray(1, 7);
+
+const assertEqual = function (actual, expected){
+  if (actual === expected) {
+    return console.log(`${String.fromCodePoint('0x2705')}  Assertion Passed: ${actual} === ${expected} `)
+  } else {
+    return console.log ( `${String.fromCodePoint('0x274c')} Assertion Failed: ${actual} === ${expected} `)
+  }
+}
+
+//assertEqual([1, 2, 3], [1, 2, 3]);
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 7);
