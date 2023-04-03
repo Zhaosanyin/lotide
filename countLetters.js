e16901aaebc38ecb6930c71a9994999fe9ef2dba
zhaosanyin <zhaosan.yin@gmail.com>
2023-04-03 02:21:58 +0000

    countLetter

---
+++ countLetters.js
@@ -0,0 +1,20 @@
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     return (console.log(String.fromCodePoint('0x2705') + "Assertion Passed:" + actual + " === " + expected));
+//   } else {
+//     return (console.log(String.fromCodePoint('0x274c') + "Assertion failed:" + actual + " !=== " + expected));
+//   }
+// };
+
+const countLetters = function(str) {
+  const result = {};
+  for (let item of str) {
+    if (result[item]) {
+      result[item] ++;
+    } else {
+      result[item] = 1;
+    }
+  } return result;
+};
+
+console.log(countLetters("LHL"));
