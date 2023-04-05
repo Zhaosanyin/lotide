d3837d3191c208e6beb37ba1cf3e283241e11d08
zhaosanyin <zhaosan.yin@gmail.com>
2023-04-05 03:31:07 +0000

    map callback function

---
+++ map.js
@@ -0,0 +1,15 @@
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+    // console.log ('item BEFORE: ', item);
+    // console.log ('item AFTER ', callback (item));
+    // console.log('---');
+  }
+  return results;
+};
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+// ['g', 'c', 't', 'm', 't']
