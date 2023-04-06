0b2e49b7f8bcc02d8127b32e3b9da1d4ab269b83
zhaosanyin <zhaosan.yin@gmail.com>
2023-04-06 03:48:27 +0000

    callback function in object

---
+++ findKey.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return (console.log(String.fromCodePoint('0x2705') + "Assertion Passed:" + actual + " === " + expected));
+  } else {
+    return (console.log(String.fromCodePoint('0x274c') + "Assertion failed:" + actual + " !=== " + expected));
+  }
+};
+
+const findKey = function (obj, callback) {
+
+  for (let key in obj) {
+    if (callback(obj[key])) {   //it is no easy to understand why using it to judgement
+      return key;
+
+    }
+  }  return undefined;
+}
+
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+console.log(result1);
+assertEqual(result1, "noma");
