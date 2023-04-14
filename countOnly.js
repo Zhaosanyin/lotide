acbce1c46a85c9aad842b2ec1c10d8b18711da44
zhaosanyin <zhaosan.yin@gmail.com>
2023-04-14 02:48:56 +0000

    countOnly update

--- countOnly.js
+++ countOnly.js
@@ -1,25 +1,15 @@
 
 
 
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     return (console.log(String.fromCodePoint('0x2705') + "Assertion Passed:" + actual + " === " + expected));
-//   } else {
-//     return (console.log(String.fromCodePoint('0x274c') + "Assertion failed:" + actual + " !=== " + expected));
-//   }
-// };
-
-const countOnly = function(allItems, itemsToCount) {
-  const results = {};
-  allItems.forEach(str => {
-    const filtered = itemsToCount.filter(elem => elem === str);
-    const conut = filtered.length;
-    results[str] = (results [str] || 0) + conut;
-  });
-  return results;
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return (console.log(String.fromCodePoint('0x2705') + "Assertion Passed:" + actual + " === " + expected));
+  } else {
+    return (console.log(String.fromCodePoint('0x274c') + "Assertion failed:" + actual + " !=== " + expected));
+  }
 };
 
-// && results [items]== itemsToCount[items]
+
 const firstNames = [
   "Karl",
   "Salima",
@@ -32,14 +22,52 @@ const firstNames = [
   "Joe"
 ];
 
-const toCount = { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false };
-const toCountKey = Object.keys(toCount);
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+
+  return results;
+};
+
+const itemToCount = { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false };
 
 
-console.log(countOnly(firstNames, toCountKey));
+console.log(countOnly(firstNames, itemToCount));
 
 
-//assertEqual(result1["Jason"], 1);
-//assertEqual(result1["Karima"], undefined);
-//assertEqual(result1["Fang"], 2);
-//assertEqual(result1["Agouhanna"], undefined);
+// assertEqual(result1["Jason"], 1);
+// assertEqual(result1["Karima"], undefined);
+// assertEqual(result1["Fang"], 2);
+// assertEqual(result1["Agouhanna"], undefined);
+
+
+
+
+
+
+
+
+//the below is the code what i think too much. keep for record!!
+
+
+
+// const countOnly = function(allItems, itemsToCount) {
+//   const results = {};
+//   allItems.forEach(str => {
+//     const filtered = itemsToCount.filter(elem => elem === str);
+//     const conut = filtered.length;
+//     results[str] = (results [str] || 0) + conut;
+//   });
+//   return results;
+// };
+
+// && results [items]== itemsToCount[items]
