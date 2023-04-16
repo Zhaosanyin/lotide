91c97bd83c1951e918b92d86130924c4bc9b90ef
zhaosanyin <zhaosan.yin@gmail.com>
2023-04-16 00:40:37 +0000

    shorthand porperty value shorthand

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
 
-module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
-};
+// module.exports = {
+//   head:   head,
+//   tail:   tail,
+//   middle: middle
+// };
+
+module.exports ={
+  head, tail, middle
+}
+
+
+module.eports = {
+  head, tail, middle
+}
